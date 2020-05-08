1c767a8efdd189a4d04a0596089336a2adf1df18
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2020-05-08 21:48:05 +0200

    next

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,87 @@
 //use std::thread;
 use std::time::Duration;
 
-#[tokio::main]
-async fn main() -> redis::RedisResult<()> {
+// #[tokio::main]
+// async fn main() {
 
-    let client = redis::Client::open("redis://10.110.0.9:6379").unwrap();
+//     let addr = "10.110.0.9:6379".parse().unwrap();
 
-    let connection_consumer = client.get_async_connection().await?;
+//     let client = redis_async::client::pubsub_connect(&addr)
+//         .await
+//         .expect("Cannot connect to Redis");
+
+//     //let connection_consumer = client.get_async_connection().await?;
     
-    tokio::spawn(async move {
-        let mut pubsub = connection_consumer.into_pubsub();
+//     tokio::spawn(async move {
+//         //let mut pubsub = connection_consumer.into_pubsub();
         
-        pubsub.psubscribe("*").await;
+//         let mut stream = client
+//                     .subscribe("*")
+//                     .await.unwrap();
 
-        let mut pubsub_stream = pubsub.on_message();
+//         //pubsub.psubscribe("*").await;
 
-        loop {
-            use futures_util::StreamExt as _;
+//         //let mut pubsub_stream = pubsub.on_message();
+//         use futures::StreamExt;
+//         println!("Redis subscription ok");
 
-            let pubsub_msg = pubsub_stream.next().await.unwrap();
+//         loop {
+//             let message = stream.next().await;
 
-            let payload = pubsub_msg.get_payload::<String>();
+//             match message {
+//                 Some(_message) => {
+//                     println!("message ...");
+//                 },
+//                 None => {
+//                     break;
+//                 }
+//             }
+//         }
+//     });
 
-            match payload {
-                Ok(payload) => {
-                    println!("payload = {}", payload);
-                },
-                Err(err) => {
-                    println!("payload err {:?}", err);
-                }
-            };
+//     tokio::time::delay_for(Duration::from_secs(2000)).await;
 
-            println!("channel {}", pubsub_msg.get_channel_name());
-        }
+//     //Ok(())
+// }
+
+
+
+
+fn main() {
+
+    smol::run(async {
+        let client = redis::Client::open("redis://10.110.0.9:6379").unwrap();
+
+        let connection_consumer = client.get_async_connection().await.unwrap();
+
+        tokio::spawn(async move {
+            let mut pubsub = connection_consumer.into_pubsub();
+            
+            pubsub.psubscribe("*").await.unwrap();
+
+            let mut pubsub_stream = pubsub.on_message();
+
+            loop {
+                use futures_util::StreamExt as _;
+
+                let pubsub_msg = pubsub_stream.next().await.unwrap();
+
+                let payload = pubsub_msg.get_payload::<String>();
+
+                match payload {
+                    Ok(payload) => {
+                        println!("payload = {}", payload);
+                    },
+                    Err(err) => {
+                        println!("payload err {:?}", err);
+                    }
+                };
+
+                println!("channel {}", pubsub_msg.get_channel_name());
+            }
+        });
+
+        tokio::time::delay_for(Duration::from_secs(2000)).await;
     });
-
-    tokio::time::delay_for(Duration::from_secs(2000)).await;
-
-    Ok(())
 }
 
